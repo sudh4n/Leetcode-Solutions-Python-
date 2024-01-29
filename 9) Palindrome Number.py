f26214b3bb3920e61d554e79daab9c845e6005c6
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2024-01-29 23:22:26 +0530

    9) Palindrome Number with Python

--- 9) Palindrome Number.py
+++ 9) Palindrome Number.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        if x < 0:
-            return False
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    if x < 0:
+      return False
 
-        rev = 0
-        copy = x
-        while copy != 0:
-            rev = rev*10 + copy%10
-            copy = copy/10
-        if rev == x:
-            return True
-        else:
-            return False
+    rev = 0
+    y = x
+
+    while y:
+      rev = rev * 10 + y % 10
+      y //= 10
+
+    return rev == x
